a31d151d6f53b0731f9f445227a1cccee9321244
王佳欣 <1974740999@qq.com>
2018-02-05 22:29:53 +0800

    Revert "add koa get、post examples"
    This reverts commit e3affcc3ace89a1c73cb39d77011917b3bfe791d.

--- routes/routers.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')();
-const loginControl = require('../controllers/login');
-
-router.get('/api/test/:name', loginControl.test);
-router.post('/api/signin', loginControl.signin);
-
-
-module.exports = router;
